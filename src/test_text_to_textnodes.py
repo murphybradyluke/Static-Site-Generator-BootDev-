529059cc4914a98fb950a9d46a8294e33c083222
Brady Murphy <murphy.bradyluke@gmail.com>
2026-09-19 00:00:10 -0500

    Finished text_to_textnodes() but tests are minimal

---
+++ src/test_text_to_textnodes.py
@@ -0,0 +1,23 @@
+import unittest
+
+
+from text_to_textnodes import text_to_textnodes
+from textnode import TextNode, TextType
+from splitdelim import split_nodes_delimiter
+from splitnodes import split_nodes_image, split_nodes_link
+
+class TestTextToTextNodes(unittest.TestCase):
+    def test_text_to_textnodes(self):
+        node = text_to_textnodes("This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)")
+        self.assertEqual(node, [
+            TextNode("This is ", TextType.TEXT),
+            TextNode("text", TextType.BOLD),
+            TextNode(" with an ", TextType.TEXT),
+            TextNode("italic", TextType.ITALIC),
+            TextNode(" word and a ", TextType.TEXT),
+            TextNode("code block", TextType.CODE),
+            TextNode(" and an ", TextType.TEXT),
+            TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+            TextNode(" and a ", TextType.TEXT),
+            TextNode("link", TextType.LINK, "https://boot.dev"),
+        ])
